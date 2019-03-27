afcc1b5e6e6a89c7f3085c635448952c02e6d21a
MeenaKrs <46361811+MeenaKrs@users.noreply.github.com>
2019-03-27 16:52:28 -0700

    update

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,56 +1,98 @@
+//Global variables
 var w = 0, l = 0;
 var won = document.getElementById("winningSound");
 var gameOver = document.getElementById("gameOverSound");
-var words = ['india', 'italy', 'japan', 'korea', 'sweden', 'spain', 'thailand', 'usa', 'vietnam', 'australia', 'brazil', 'belgium', 'china', 'cuba', 'canada', 'denmark', 'egypt', 'france'];
+var wordList = ['india', 'italy', 'japan', 'korea', 'sweden', 'spain', 'thailand', 'usa', 'vietnam', 'australia', 'brazil', 'belgium', 'china', 'cuba', 'canada', 'denmark', 'egypt', 'france'];
 var guessL = [];
 var possible = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-var word = words[Math.floor(Math.random() * words.length)];
-console.log("Got the word: " + word);
-var end = false;
-var trii = 10;
-tries.textContent = "Remaining Tries:" + trii;
-whatToDisplay = [];
-for (var i = 0; i < word.length; i++) {
-    whatToDisplay[i] = false;
+var end, trii, whatToDisplay, poss;
+var newWordFlag = true;
+
+//Reset for a new game
+function newGame() {
+    var w = wordList[Math.floor(Math.random() * wordList.length)];
+    end = false;
+    trii = 10;
+    guessL = [];
+    tries.textContent = "Remaining Tries:" + trii;
+    whatToDisplay = [];
+    actualWord.textContent = "";
+    res.textContent = ("Keep playing!!");
+    for (let i = 0; i < w.length; i++) {
+        whatToDisplay[i] = false;
+    }
+    return w;
 }
+
+//Update field
+function updateField(input) {
+    if (!guessL.includes(input)) {
+        for (let j = 0; j < word.length; j++) {
+            if (input == word[j]) {
+                whatToDisplay[j] = true;
+            }
+        }
+        guessL.push(input);
+        trii -= 1;
+    }
+}
+
+function newFieldStr() {
+    let tmp = "";
+    for (var k = 0; k < word.length; k++) {
+        if (whatToDisplay[k]) {
+            tmp += word[k];
+        } else {
+            tmp += ' _';
+        }
+    }
+    return tmp;
+}
+
+//User won
+function winwin() {
+    res.textContent = ("You win !! Hit enter to continue");
+    won.play();
+    w++;
+    newWordFlag = true;
+    end = true;
+}
+
+//User lost
+function gOver() {
+    gameOver.play();
+    l++;
+    newWordFlag = true;
+    res.textContent = ("You failed :(");
+    actualWord.textContent = ("Anwer is " + word);
+    end = true;
+}
+
+//Action Listener
 document.onkeyup = function (event) {
+    if (newWordFlag) {
+        word = newGame();
+        newWordFlag = false;
+    }
     if (!end) {
         var userInput = event.key;
+        userInput = userInput.toLowerCase();
         if (possible.includes(userInput)) {
-            if (!guessL.includes(event.key)) {
-                for (var j = 0; j < word.length; j++) {
-                    if (userInput == word[j]) {
-                        whatToDisplay[j] = true;
-                    }
-                }
-                guessL.push(event.key);
-                trii -= 1;
-            }
+            updateField(userInput);
         }
-        var tmp = "";
-        for (var k = 0; k < word.length; k++) {
-            if (whatToDisplay[k]) {
-                tmp += word[k];
-            } else {
-                tmp += ' _';
-            }
-        }
-        field.textContent = tmp;
-        guessed.textContent = "Guessed letters: " + guessL;
-        tries.textContent = "Remaining Tries:" + trii;
+        var newField = newFieldStr();
+        field.textContent = newField;
+        document.getElementById("hint").src = "assets/images/" + word + ".jpg";
+        document.getElementById("guessed").innerText = "Guessed letters: " + guessL;
+        document.getElementById("tries").innerText = "Remaining Tries: " + trii;
+        // guessed.textContent = "Guessed letters: " + guessL;
+        // tries.textContent = "Remaining Tries: " + trii;
         if ((trii < 1) && (whatToDisplay.includes(false))) {
-            res.textContent = ("Game Over!!");
-            gameOver.play();
-            l++;
-            actualWord.textContent = ("Actual word is " + word);
-            end = true;
+            gOver();
         } else if (!whatToDisplay.includes(false)) {
-            res.textContent = ("You win !!");
-            won.play();
-            w++;
-            end = true;
+            winwin();
         }
-        win.textContent = "Wins:" + w;
-        los.textContent = "Loses:" + l;
+        win.textContent = "Wins: " + w;
+        los.textContent = "Loses: " + l;
     }
 }
